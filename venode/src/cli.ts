3841111a1861db8875eaf66b79e540e92ec878a1
aslemammad <zorofight94@gmail.com>
2022-03-23 02:07:09 +0430

    update

--- venode/src/cli.ts
+++ venode/src/cli.ts
@@ -34,9 +34,67 @@ import { Extension, Meta } from "./types";
           const { pathname, href } = new URL(module[0]);
 
           const newId = path.join(path.dirname(pathname), id);
-          console.log('newId', newId)
+          console.log("newId", newId);
 
           return href.replace(pathname, newId);
+        },
+      },
+      {
+        enforce: "pre",
+        name: "venode",
+        async resolveId(id, importer) {
+          console.log("resolveId", id, importer);
+          if (!id.startsWith("http")) {
+            return null;
+          }
+          if (handledModules.has(id)) {
+            return { id: handledModules.get(id) as string, external: false };
+          }
+
+          const url = new URL(id);
+          const meta = path.join(dest, urlToFilename(url) + ".meta");
+
+          let metaContent: Meta | undefined;
+          try {
+            metaContent = JSON.parse(await fs.readFile(meta, "utf8"));
+          } catch {}
+          if (metaContent) {
+            handledModules.set(id, metaContent.path);
+            return { id: metaContent.path, external: false };
+          }
+
+          console.log(c.green(`Download ${c.reset(id)}`));
+          try {
+            debugger;
+            const res = await fetch(id);
+            const mimeExtension = mime.extension(
+              res.headers.get("content-type") || ""
+            );
+            const ext = resolveExtension(url.href) || "." + mimeExtension;
+
+            if (!isValidExtension(ext)) {
+              throw new Error(`Unknown extension for ${id}`);
+            }
+            const filename = filenameWithExtension(url, ext as Extension);
+            const text = await res.text();
+
+            const destFilename = path.join(dest, filename);
+
+            await mkdirp(path.dirname(destFilename));
+            await fs.writeFile(destFilename, text);
+            await fs.writeFile(
+              meta,
+              JSON.stringify({
+                path: destFilename,
+                hash: path.basename(urlToFilename(url)),
+              })
+            );
+            handledModules.set(id, destFilename);
+            return { id: destFilename, external: false };
+          } catch (e) {
+            console.error(c.red("Failed"), id, e);
+            process.exit(1);
+          }
         },
       },
     ],
@@ -54,58 +112,7 @@ import { Extension, Meta } from "./types";
       return node.fetchModule(id);
     },
     async resolveId(id, importer) {
-      console.log('resolveId', id, importer)
-      if (!id.startsWith("http")) {
-        return node.resolveId(id, importer);
-      }
-      if (handledModules.has(id)) {
-        return { id: handledModules.get(id) as string, external: false };
-      }
-
-      const url = new URL(id);
-      const meta = path.join(dest, urlToFilename(url) + ".meta");
-
-      let metaContent: Meta | undefined;
-      try {
-        metaContent = JSON.parse(await fs.readFile(meta, "utf8"));
-      } catch {}
-      if (metaContent) {
-        handledModules.set(id, metaContent.path);
-        return { id: metaContent.path, external: false };
-      }
-
-      console.log(c.green(`Download ${c.reset(id)}`));
-      try {
-        debugger;
-        const res = await fetch(id);
-        const mimeExtension = mime.extension(
-          res.headers.get("content-type") || ""
-        );
-        const ext = resolveExtension(url.href) || "." + mimeExtension;
-
-        if (!isValidExtension(ext)) {
-          throw new Error(`Unknown extension for ${id}`);
-        }
-        const filename = filenameWithExtension(url, ext as Extension);
-        const text = await res.text();
-
-        const destFilename = path.join(dest, filename);
-
-        await mkdirp(path.dirname(destFilename));
-        await fs.writeFile(destFilename, text);
-        await fs.writeFile(
-          meta,
-          JSON.stringify({
-            path: destFilename,
-            hash: path.basename(urlToFilename(url)),
-          })
-        );
-        handledModules.set(id, destFilename);
-        return { id: destFilename, external: false };
-      } catch (e) {
-        console.error(c.red("Failed"), id, e);
-        process.exit(1);
-      }
+      return node.resolveId(id, importer);
     },
   });
 
