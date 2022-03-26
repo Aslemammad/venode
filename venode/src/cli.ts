d3c1590faa8b0f64f91358496eaaf4d419c011f9
aslemammad <zorofight94@gmail.com>
2022-03-26 11:59:09 +0430

    update

--- venode/src/cli.ts
+++ venode/src/cli.ts
@@ -94,6 +94,7 @@ if (isVendor && importMap) {
           const module = [...handledModules.entries()].find(
             (item) => item[1] === importer
           );
+          console.log(handledModules)
           console.log('here', module, id, importer)
           if (!module) {
             return null;
